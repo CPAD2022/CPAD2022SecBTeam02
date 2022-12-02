bd30b66fad3698919e5bc82369bcd57fcccbdffd
AkshataPuranikbits <2021SP93025@wilp.bits-pilani.ac.in>
2022-12-02 17:16:33 +0530

    Adding starter.js

---
+++ src/pages/Starter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { } from 'react-native';
+
+
+export default class GetAttendance extends React.Component {
+    render(){
+        return(
+            <Text>Hello</Text>
+        )
+    }
+}
