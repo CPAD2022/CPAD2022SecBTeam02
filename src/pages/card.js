d1cfa45e1adcf0dc830f54a883856973ae17338d
PoojaMagadum <2021sp93050@wilp.bits-pilani.ac.in>
2022-12-02 10:52:57 +0530

    style

--- src/pages/card.js
+++ src/pages/card.js
@@ -30,5 +30,43 @@ export default class Card extends Component {
             </View>
         );
     }
-
 }
+
+    const styles = StyleSheet.create({
+        maincontainer:{
+            flex: 1,
+            justifyContent: 'center',
+            backgroundColor:'#899097',
+            paddingTop: ( Platform.OS === 'ios' ) ? 20 : 0,
+            
+        },
+        buttonpresent:{
+          width:300,
+          backgroundColor: "green",
+          borderRadius:25,
+          marginVertical:10,
+          paddingVertical:10,
+          marginHorizontal:25,
+      },buttontext:{
+        fontSize:20,
+        fontWeight:'500',
+        color:'#ffffff',
+        textAlign:'center',
+      },
+      buttonabsent:{
+        width:300,
+        backgroundColor: "red",
+        borderRadius:25,
+        marginVertical:10,
+        paddingVertical:10,
+        marginHorizontal:23,
+    },
+    text:{
+        fontSize:25,
+        fontWeight:'500',
+        color:'#ffffff',
+        marginHorizontal:25,
+        marginVertical:10,
+      }
+      });
+
