556e4de6c2ef1342c98228ff45ad9e06aa32cd31
PoojaMagadum <2021sp93050@wilp.bits-pilani.ac.in>
2022-12-02 15:37:55 +0530

    collection of attendance

--- src/pages/Guestattendance.js
+++ src/pages/Guestattendance.js
@@ -24,4 +24,41 @@ export default class GuestAttendance extends React.Component {
             attendance: []
         }
     }
+    async getAttendance(){
+        const batchReference = db.collection('batch').doc(this.state.student.batch)
+        const subjects = await db.collection("subject").where("batch", "==", batchReference).get()
+        const student = await db.collection('student').doc(this.state.student.id).get()
+        for(let i=0;i < subjects.docs.length; i++){
+            let v = subjects.docs[i]
+            const attendanceRef = await db.collection('attendance').where("subject", "==", v.ref).where("student", "==", student.ref).get()
+            const attendance = attendanceRef.docs
+            let total = attendance.length
+            let present = attendance.filter(v => v.get("status")).length
+            this.state.attendance.push({
+                "id": v.id,
+                "name": v.get("name"),
+                "total": total,
+                "present": present,
+                "percentage": `${((present / (total > 0 ? total : 1)) * 100).toFixed(2) }%`
+            })
+        }
+    }
+
+    componentDidMount(){
+        db.collection('student').doc(this.props.student).get().then((s) => {
+            this.setState({
+                student: {
+                    "id": s.id,
+                    name: s.get("name"),
+                    usn: s.get("usn"),
+                    batch: s.get("batch").id,
+                }
+            })
+            this.getAttendance().then(() => {
+               this.setState({
+                    isLoading: false
+               })
+            })
+        })
+    }
 }
