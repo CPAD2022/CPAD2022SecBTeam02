2fbf244d4ee9cf4e69518b225303b93325a6c349
AkshataPuranikbits <2021SP93025@wilp.bits-pilani.ac.in>
2022-12-02 16:04:02 +0530

    buttons,dropdown,css added

--- src/pages/SelectInternalsMarks.js
+++ src/pages/SelectInternalsMarks.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import {View,StyleSheet,TouchableOpacity,Text,Dimensions, ActivityIndicator,ImageBackground,Platform} from 'react-native';
 import {Actions} from 'react-native-router-flux';
@@ -72,4 +73,160 @@ export default class SelectInternalsForMarks extends Component {
         "label": e.name
       }
     } )
-  }}
+  }
+
+  componentDidMount(){
+    db.collection("batch").get().then(snapshot => {
+      let batches = []
+      snapshot.docs.forEach((doc) => {
+        batches.push(
+          {
+            "name" : doc.get("name"),
+            "id": doc.id
+          }
+        )
+        
+      })
+      this.setState(
+        {
+          batches: batches,
+          selectedBatch: batches[0].id,
+          isLoading: false
+        }
+      )
+      if(batches.length > 0){
+        this.setState({
+          selectedBatch: batches[0].id
+        })
+        this.retrieveSubjects(batches[0].id)
+      }
+
+    })
+  }
+  
+  
+  render() {
+  return(
+  <View style={styles.maincontainer}>
+    <View >
+      <ImageBackground 
+          style={styles.backgroundimage}
+          source={require('../images/pic.jpg')}>
+      <Text style={{
+        fontSize:33,
+        fontWeight:'bold',
+        textAlign:'center',
+        paddingVertical:110,
+        }}>
+        Select Internals
+      </Text>
+
+    {this.state.isLoading || this.state.selectedBatch == undefined  ? <ActivityIndicator size="large" color="#1273de" /> : 
+
+      <Dropdown
+      containerStyle={styles.pickerStyle}
+      style={{fontSize:23,height:35}}
+      labelFontSize={22}
+       label={'Choose batch'}
+        data={this.formatData(this.state.batches)}
+         useNativeDriver={true}
+         value={this.state.selectedBatch}
+          onChangeText={(v) => {
+           this.setState({
+              selectedBatch: v,
+              isLoading: true
+            })
+            this.retrieveSubjects(v)
+           }}
+        />
+    }
+    {this.state.isLoading || this.state.selectedSubject == undefined ? <ActivityIndicator size="small" color="#1273de" /> : 
+
+    <Dropdown
+    containerStyle={styles.pickerStyle}
+    style={{fontSize:23,height:35}}
+    labelFontSize={22}
+    label={'Choose subject'}
+    data={this.formatData(this.state.subjects)}
+    useNativeDriver={true}
+    value={this.state.selectedSubject}
+    onChangeText={(v) => {
+      this.setState({
+        selectedSubject: v,
+        isLoading: true
+      })
+      this.state["selectedInternal"] = "No internals present"
+      this.retrieveInternals(v)
+    }}
+    />
+
+  }
+
+    {this.state.isLoading ? <ActivityIndicator size="small" color="#1273de" /> : 
+    
+    <Dropdown
+    containerStyle={styles.pickerStyle}
+    style={{fontSize:23,height:35}}
+    labelFontSize={22}
+    label={'Choose Internals'}
+    data={this.formatData(this.state.internals)}
+    useNativeDriver={true}
+    value={this.state.selectedInternal}
+    onChangeText={(v) => {
+      this.setState({
+        selectedInternal: v
+      })
+    }
+    }
+/>
+   
+  }
+    
+   
+   <TouchableOpacity  style={styles.button} 
+      onPress={() => Actions.getmarks({internal: this.state.selectedInternal})}>
+      <Text style={styles.buttontext}>
+        Submit
+      </Text>
+    </TouchableOpacity>
+    </ImageBackground>
+    </View>
+  </View>
+  ) }}
+
+const styles = StyleSheet.create({
+  maincontainer:{
+    paddingTop: ( Platform.OS === 'ios' ) ? 20 : 0,
+    alignItems: 'center',
+  },
+  button:{
+    textAlign: "center",
+    backgroundColor: "#202824",
+    borderRadius:25,
+    marginVertical:100,
+    paddingVertical:12,
+},
+buttontext:{
+  fontSize:22,
+  fontWeight:'bold',
+  color:'#ffffff',
+  textAlign:'center',
+    
+},
+backgroundimage:{
+  flex:1,
+  width: '130%',
+  height: '100%',
+  opacity: 0.9,
+  paddingHorizontal:60,
+},
+pickerStyle:{ 
+  color: 'red',
+  width: width * 0.8,
+  alignSelf: "stretch",
+  justifyContent:'center',
+  
+ 
+}  
+
+});
