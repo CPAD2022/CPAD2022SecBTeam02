b704f618a855e0467ccf5e50aba66e706248e5a4
PoojaMagadum <2021sp93050@wilp.bits-pilani.ac.in>
2022-12-02 15:46:27 +0530

    Add style to homepage

--- src/pages/guesthomepage.js
+++ src/pages/guesthomepage.js
@@ -96,3 +96,41 @@ export default class Guesthomepage extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    maincontainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#899097',
+        paddingTop: ( Platform.OS === 'ios' ) ? 20 : 0,
+    },
+    button:{
+        flexDirection:'row',
+        alignItems: 'center',
+        width:270,
+        backgroundColor: "#44584e",
+        borderWidth: .5,
+        borderRadius:30,
+        height: 70,
+        margin:7,
+    },
+    buttontext:{
+        fontSize:22,
+        fontWeight:'bold',
+        color:'red',
+        marginBottom : 4,
+        paddingHorizontal:35, 
+        
+       
+    },
+    backgroundimage:{
+        flex: 1,
+        width: null,
+        height: null,
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: 0.95,
+        paddingHorizontal:40,  
+    },
+});
